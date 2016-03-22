6acf4aba26817d015d74ca06eeebca1325c9d6ef
Gord Pearson <gord.pearson@shopify.com>
2016-03-22 10:07:12 -0400

    Move helper functions into transformer; destructure arguments

--- packages/shopify-codemod/transforms/conditional-assign-to-if-statement.js
+++ packages/shopify-codemod/transforms/conditional-assign-to-if-statement.js
@@ -1,60 +1,57 @@
-function isConditionalAssignment(node) {
-  const isUsingOr = (node.operator === '||');
-  const isLeftCheckingVariable = (node.left.type === 'MemberExpression') || (node.left.type === 'Identifier');
-  const isRightAssignment = (node.right.type === 'AssignmentExpression');
+export default function conditionalAssignToIfStatement({source}, {jscodeshift: j}, {printOptions = {quote: 'single'}}) {
+  function isConditionalAssignment(node) {
+    const isUsingOr = (node.operator === '||');
+    const isLeftCheckingVariable = (node.left.type === 'MemberExpression') || (node.left.type === 'Identifier');
+    const isRightAssignment = (node.right.type === 'AssignmentExpression');
 
-  return isUsingOr && isLeftCheckingVariable && isRightAssignment;
-}
+    return isUsingOr && isLeftCheckingVariable && isRightAssignment;
+  }
 
-function isMemberEqual(aNode, bNode) {
-  return (
-    aNode.object.type === bNode.object.type &&
-    aNode.object.name === bNode.object.name &&
-    aNode.property.type === bNode.property.type &&
-    aNode.property.name === bNode.property.name
-  );
-}
+  function isMemberEqual(aNode, bNode) {
+    return (
+      aNode.object.type === bNode.object.type &&
+      aNode.object.name === bNode.object.name &&
+      aNode.property.type === bNode.property.type &&
+      aNode.property.name === bNode.property.name
+    );
+  }
 
-function isIdentifierEqual(aNode, bNode) {
-  return (
-    aNode.type === bNode.type &&
-    aNode.name === bNode.name
-  );
-}
+  function isIdentifierEqual(aNode, bNode) {
+    return (
+      aNode.type === bNode.type &&
+      aNode.name === bNode.name
+    );
+  }
 
-function isAssigningToConditionObject(node) {
-  const condition = node.left;
-  const assignment = node.right.left;
-  const isMemberExpression = (condition.type === 'MemberExpression');
+  function isAssigningToConditionObject(node) {
+    const condition = node.left;
+    const assignment = node.right.left;
+    const isMemberExpression = (condition.type === 'MemberExpression');
 
-  return isMemberExpression
-    ? isMemberEqual(condition, assignment)
-    : isIdentifierEqual(condition, assignment);
-}
+    return isMemberExpression
+      ? isMemberEqual(condition, assignment)
+      : isIdentifierEqual(condition, assignment);
+  }
 
-function blockifyAssignment(j, node) {
-  const assignment = j.expressionStatement(node);
-  const assignmentBlock = j.blockStatement([assignment]);
+  function blockifyAssignment(node) {
+    const assignment = j.expressionStatement(node);
+    const assignmentBlock = j.blockStatement([assignment]);
 
-  return assignmentBlock;
-}
+    return assignmentBlock;
+  }
 
-function negateExistenceCondition(j, oldCondition) {
-  const booleanCheck = j.callExpression(j.identifier('Boolean'), [oldCondition]);
-  const newCondition = j.unaryExpression('!', booleanCheck);
+  function negateExistenceCondition(oldCondition) {
+    const booleanCheck = j.callExpression(j.identifier('Boolean'), [oldCondition]);
+    const newCondition = j.unaryExpression('!', booleanCheck);
 
-  return newCondition;
-}
+    return newCondition;
+  }
 
-export default function conditionalAssignToIfStatement(file, api, options) {
-  const j = api.jscodeshift;
-  const printOptions = options.printOptions || {quote: 'single'};
-
-  return j(file.source)
+  return j(source)
     .find(j.LogicalExpression, (node) => isConditionalAssignment(node) && isAssigningToConditionObject(node))
     .replaceWith((path) => {
-      const condition = negateExistenceCondition(j, path.node.left);
-      const assignment = blockifyAssignment(j, path.node.right);
+      const condition = negateExistenceCondition(path.node.left);
+      const assignment = blockifyAssignment(path.node.right);
 
       return j.ifStatement(condition, assignment);
     })
