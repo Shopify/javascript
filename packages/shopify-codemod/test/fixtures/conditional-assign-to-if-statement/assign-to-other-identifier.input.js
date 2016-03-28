2886936a1ceffe7c2831e6f1dae3e1ff9fd789b7
Gord Pearson <gord.pearson@shopify.com>
2016-03-28 15:16:08 -0400

    Transform conditional assignments to `if` blocks
    Note that this does not handle conditional assignments that return a value, or that immediately call into the assigned value.

---
+++ packages/shopify-codemod/test/fixtures/conditional-assign-to-if-statement/assign-to-other-identifier.input.js
@@ -0,0 +1,7 @@
+this.foo || (this.bar = 'lol');
+self.foo || (self.bar = 'lols');
+foo || (bar = 'lols');
+
+this.foo && (this.bar = 'lol');
+self.foo && (self.bar = 'lol');
+foo && (bar = 'lol');
---
+++ packages/shopify-codemod/test/fixtures/conditional-assign-to-if-statement/assign-to-other-identifier.output.js
@@ -0,0 +1,23 @@
+if (!this.foo) {
+  this.bar = 'lol';
+}
+
+if (!self.foo) {
+  self.bar = 'lols';
+}
+
+if (!foo) {
+  bar = 'lols';
+}
+
+if (this.foo) {
+  this.bar = 'lol';
+}
+
+if (self.foo) {
+  self.bar = 'lol';
+}
+
+if (foo) {
+  bar = 'lol';
+}
