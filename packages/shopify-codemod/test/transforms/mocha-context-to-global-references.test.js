1ce0138ab7eecda817f16411d35577b5ef8e9d9b
Chris Sauve <chris.sauve@shopify.com>
2016-04-08 13:20:43 -0400

    Split out the tests

--- packages/shopify-codemod/test/transforms/mocha-context-to-global-references.test.js
+++ packages/shopify-codemod/test/transforms/mocha-context-to-global-references.test.js
@@ -2,6 +2,14 @@ import 'test-helper';
 import mochaContextToGlobalReferences from 'mocha-context-to-global-references';
 
 describe('mochaContextToGlobalReferences', () => {
+  it('renames the member expression to use a test global', () => {
+    expect(mochaContextToGlobalReferences).to.transform('mocha-context-to-global-references/rename', {
+      testContextToGlobals: {
+        sinon: {properties: ['spy', 'stub', 'server']},
+      }
+    });
+  });
+
   it('replaces a contextual reference to a global reference', () => {
     expect(mochaContextToGlobalReferences).to.transform('mocha-context-to-global-references/replace', {
       testContextToGlobals: {
@@ -14,12 +22,6 @@ describe('mochaContextToGlobalReferences', () => {
           properties: ['shouldBeReplaced', 'mustBeReplaced'],
           replace: true,
         }
-      }
-    });
-
-    expect(mochaContextToGlobalReferences).to.transform('mocha-context-to-global-references/rename', {
-      testContextToGlobals: {
-        sinon: {properties: ['spy', 'stub', 'server']},
       }
     });
   });
