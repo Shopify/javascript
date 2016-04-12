64d619d6fde011bb61d4ccfc50c8b2d7c9aa5f2c
Chris Sauve <chris.sauve@shopify.com>
2016-04-11 20:47:34 -0400

    Add prefer-early-return rule

--- packages/eslint-plugin-shopify/lib/config/rules/shopify.js
+++ packages/eslint-plugin-shopify/lib/config/rules/shopify.js
@@ -3,4 +3,6 @@ module.exports = {
   'shopify/require-flow': 'off',
   // Requires (or disallows) assignments of binary, boolean-producing expressions to be wrapped in parentheses.
   'shopify/binary-assignment-parens': ['warn', 'always'],
+  // Prefer early returns over full-body conditional wrapping in function declarations.
+  'shopify/prefer-early-return': ['warn', {maximumStatements: 1}],
 };
