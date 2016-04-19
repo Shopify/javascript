b62e7c9116e29c42113037594b046c8774658101
Chris Sauve <chris.sauve@shopify.com>
2016-04-19 10:14:51 -0400

    Fix flowtype config

--- packages/eslint-plugin-shopify/lib/config/rules/flowtype.js
+++ packages/eslint-plugin-shopify/lib/config/rules/flowtype.js
@@ -4,11 +4,11 @@ module.exports = {
   // Requires that all function parameters have type annotations.
   'flowtype/require-parameter-type': 'warn',
   // Requires that functions have return type annotation.
-  'flowtype/require-return-type': ['always', {annotateUndefined: 'never'}],
+  'flowtype/require-return-type': ['warn', 'always', {annotateUndefined: 'never'}],
   // Enforces consistent spacing after the type annotation colon.
-  'flowtype/space-after-type-colon': 'always',
+  'flowtype/space-after-type-colon': ['warn', 'always'],
   // Enforces consistent spacing before the type annotation colon.
-  'flowtype/space-before-type-colon': 'never',
+  'flowtype/space-before-type-colon': ['warn', 'never'],
   // Enforces a consistent naming pattern for type aliases.
   'flowtype/type-id-match': ['warn', '^([A-Z][a-z0-9]+)+Type$'],
 };
