af778e68b74702850450fa608b1fb81956ebe438
Andy Fang <fang@Andys-MacBook-Pro-2.local>
2016-05-11 15:43:32 -0400

    fixed linting error

--- packages/shopify-codemod/transforms/remove-redundant-returns-from-addeventlistener.js
+++ packages/shopify-codemod/transforms/remove-redundant-returns-from-addeventlistener.js
@@ -1,4 +1,4 @@
-export default function transformer(file, api) {
+export default function removeAddEventListener(file, api) {
   const j = api.jscodeshift;
   return j(file.source) // parse input to AST
     .find(j.CallExpression, {
@@ -9,20 +9,20 @@ export default function transformer(file, api) {
         },
       },
     })
-    .forEach(
-      (nodes) => {
-        const functionExpression = nodes.value.arguments[1];
-        const {body} = functionExpression.body;
-        const lastStatement = body[body.length - 1];
-        if (lastStatement.type === 'ReturnStatement') {
-          if (lastStatement.argument === null) {
-            body.pop(); // remove returns with no args
-          }
-          body[body.length - 1] = j.expressionStatement(body[body.length - 1].argument);
-        } else {
-          return nodes;
+    .forEach((nodes) => {
+      const functionExpression = nodes.value.arguments[1];
+      const {body} = functionExpression.body;
+      const lastStatement = body[body.length - 1];
+      if (lastStatement.type === 'ReturnStatement') {
+        if (lastStatement.argument === null) {
+          body.pop(); // remove returns with no args
         }
+        body[body.length - 1] = j.expressionStatement(body[body.length - 1].argument);
+      } else {
+        return nodes;
       }
-    )
-    .toSource();
+      return body;
+    }
+  )
+  .toSource();
 }
