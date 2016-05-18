ae6632186572e31cc3d5f2b8f7930d021d01d838
Chris Sauve <chris.sauve@shopify.com>
2016-05-17 21:06:08 -0400

    Add an error assertion to chai-jscodeshift

--- packages/chai-jscodeshift/index.js
+++ packages/chai-jscodeshift/index.js
@@ -22,27 +22,38 @@ module.exports = function chaiJSCodeShift(options) {
     var Assertion = chaiOptions.Assertion;
     var assert = chaiOptions.assert;
 
-    function transform(fixture, transformOptions) {
-      var transformer = this._obj;
-
+    function transformFixtureWithOptions(transformer, fixture, transformOptions) {
       var inputPath = finalConfig.inputFixturePath(fixture, finalConfig.fixtureDirectory);
-      var outputPath = finalConfig.outputFixturePath(fixture, finalConfig.fixtureDirectory);
 
       var input = fs.readFileSync(inputPath, 'utf8');
-      var output = fs.readFileSync(outputPath, 'utf8').trim();
-      var transformed = transformer(
+      return transformer(
         {source: input, path: inputPath},
         {jscodeshift: jscodeshift},
         merge({}, finalConfig.transformOptions, transformOptions || {})
       ).trim();
+    }
 
+    function transform(fixture, transformOptions) {
+      var outputPath = finalConfig.outputFixturePath(fixture, finalConfig.fixtureDirectory);
+      var output = fs.readFileSync(outputPath, 'utf8').trim();
+      var transformed = transformFixtureWithOptions(this._obj, fixture, transformOptions);
       new Assertion(transformed).to.equal(output);
     }
 
+    function throwWhileTransforming(fixture, error, transformOptions) {
+      var transformer = this._obj;
+      new Assertion(function() { transformFixtureWithOptions(transformer, fixture, transformOptions); }).to.throw(error);
+    }
+
     Assertion.addMethod('transform', transform);
+    Assertion.addMethod('throwWhileTransforming', throwWhileTransforming);
 
     assert.transforms = function(transformer, fixture, transformerOptions) {
       return (new Assertion(transformer)).to.transform(fixture, transformerOptions || {});
+    };
+
+    assert.throwsWhileTransforming = function(transformer, fixture, error, transformerOptions) {
+      return (new Assertion(transformer)).to.throwWhileTransforming(fixture, error, transformerOptions);
     };
   };
 
