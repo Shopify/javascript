e3c27c8fe6114ead204aa7e6ddbb16c79271a50f
Andy Fang <andy.fang@shopify.com>
2016-05-30 13:40:13 -0400

    Fixed linting errors

--- packages/shopify-codemod/test/transforms/jquery-dollar-sign-references.test.js
+++ packages/shopify-codemod/test/transforms/jquery-dollar-sign-references.test.js
@@ -3,6 +3,6 @@ import jQueryDollarSignReferences from 'jquery-dollar-sign-references';
 
 describe('jQueryDollarSignReferences', () => {
   it('adds dollar sign to jQuery element declarations', () => {
-    expect(jQueryDollarSignReferences).to.transform('jquery-dollar-sign-references/basic')
+    expect(jQueryDollarSignReferences).to.transform('jquery-dollar-sign-references/basic');
   });
 });
--- packages/shopify-codemod/transforms/jquery-dollar-sign-references.js
+++ packages/shopify-codemod/transforms/jquery-dollar-sign-references.js
@@ -5,40 +5,44 @@ export default function jQueryDollarSignReferences({source}, {jscodeshift: j}, {
 
   sourceAST
     .find(j.VariableDeclaration, {
-    	declarations: [
-          {
-            type: 'VariableDeclarator',
-            id: {
+      declarations: [
+        {
+          type: 'VariableDeclarator',
+          id: {
+            type: 'Identifier',
+          },
+          init: {
+            type: 'CallExpression',
+            callee: {
               type: 'Identifier',
+              name: '$',
             },
-            init: {
-              type: 'CallExpression',
-              callee: {
-                type: 'Identifier',
-                name: '$'
-              }
-            }
-          }
-        ]
-  }).forEach(({node: {declarations}}) => {
-    declarations.forEach(node => {
-      toConvert.push(node.id.name);
-      node.id.name = `$${node.id.name}`;
+          },
+        },
+      ],
+    }).forEach(({node: {declarations}}) => {
+      declarations.forEach((node) => {
+        toConvert.push(node.id.name);
+        node.id.name = `$${node.id.name}`;
+      });
     });
-  });
 
   sourceAST.find(j.ExpressionStatement, {
     type: 'ExpressionStatement',
     expression: {
       type: 'CallExpression',
       callee: {
-        type: 'MemberExpression'
-      }
-    }
+        type: 'MemberExpression',
+      },
+    },
   })
   .filter(({node: {expression: {callee: {object}}}}) => toConvert.indexOf(object.name || object.callee.object.name) > -1)
   .forEach(({node: {expression: {callee: {object}}}}) => {
-    object.name ? object.name = `$${object.name}` : object.callee.object.name = `$${object.callee.object.name}`;
+    if (object.name) {
+      object.name = `$${object.name}`;
+    } else {
+      object.callee.object.name = `$${object.callee.object.name}`;
+    }
   });
 
   return sourceAST
