bd50ef87a9bf4e3a7c2b5670ba60e8bd69af74c8
Chris Sauve <chris.sauve@shopify.com>
2016-05-30 09:27:37 -0400

    Fix exclusive mocha test

--- packages/shopify-codemod/test/transforms/add-missing-parseint-radix.test.js
+++ packages/shopify-codemod/test/transforms/add-missing-parseint-radix.test.js
@@ -2,7 +2,7 @@ import 'test-helper';
 import transform from 'add-missing-parseint-radix';
 
 describe('addMissingParseintRadix', () => {
-  it.only('adds missing radix parameters where appropriate', () => {
+  it('adds missing radix parameters where appropriate', () => {
     expect(transform).to.transform('add-missing-parseint-radix/basic');
   });
 });
