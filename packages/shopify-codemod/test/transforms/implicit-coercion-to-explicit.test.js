9907452086f8eba50765273c0d04b19d5495b678
Chris Sauve <chris.sauve@shopify.com>
2016-06-01 08:52:07 -0400

    Rename transform

--- packages/shopify-codemod/test/transforms/implicit-coercion-to-explicit.test.js
+++ packages/shopify-codemod/test/transforms/implicit-coercion-to-explicit.test.js
@@ -1,8 +1,8 @@
 import 'test-helper';
-import transform from 'implicit-coercion-to-explicit';
+import implicitCoercionToExplicit from 'implicit-coercion-to-explicit';
 
 describe('implicitCoercionToExplicit', () => {
   it('transforms the most offensive implicit coercion to be explicit', () => {
-    expect(transform).to.transform('implicit-coercion-to-explicit/basic');
+    expect(implicitCoercionToExplicit).to.transform('implicit-coercion-to-explicit/basic');
   });
 });
