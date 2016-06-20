904160dc8ccba717863e55bc91de1de0824b0912
Gord Pearson <gord.pearson@shopify.com>
2016-06-20 13:41:29 -0400

    Rename `args`=>`arguments` in "before" test

--- packages/shopify-codemod/test/fixtures/arguments-to-args-spread/nested-iife.input.js
+++ packages/shopify-codemod/test/fixtures/arguments-to-args-spread/nested-iife.input.js
@@ -1,14 +1,14 @@
 function foo() {
   (function() { // Function IIFE has its own scope.
-    console.log(arguments, 'a' === args[0]);
+    console.log(arguments, 'a' === arguments[0]);
 
     (() => {
-      console.log(arguments, 'a' === args[0]);
+      console.log(arguments, 'a' === arguments[0]);
       (() => {
-        console.log(arguments, 'a' === args[0]);
+        console.log(arguments, 'a' === arguments[0]);
 
         (function() {
-          console.log(arguments, 'd' === args[0]);
+          console.log(arguments, 'd' === arguments[0]);
         })('d');
       })('c');
     })('b');
