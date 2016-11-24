07cd98e1443d7fa66af5a289a32a2572590fa4a7
Gord Pearson <gord.pearson@shopify.com>
2016-11-23 23:42:23 -0500

    Use path+replace; fix indentation

--- packages/shopify-codemod/transforms/split-if-assignments.js
+++ packages/shopify-codemod/transforms/split-if-assignments.js
@@ -3,30 +3,39 @@ export default function splitIfAssignments({source}, {jscodeshift: j}, {printOpt
   const sourceAST = j(source);
 
   // Handle else-ifs in reverse order (otherwise initial transforms orphan later paths).
-  sourceAST.find(j.IfStatement, {
-    alternate: {
-      type: j.IfStatement.name,
-      test: j.AssignmentExpression.check,
-    },
-  }).paths()
+  sourceAST
+    .find(j.IfStatement, {
+      alternate: {
+        type: j.IfStatement.name,
+        test: j.AssignmentExpression.check,
+      },
+    })
+    .paths()
     .reverse()
-    .forEach(({node}) => {
-      const elseIf = node.alternate;
-      const test = elseIf.test;
-      node.alternate = j.blockStatement([
-        j.expressionStatement(test),
-        j.ifStatement(test.left, elseIf.consequent, elseIf.alternate),
-      ]);
+    .forEach((path) => {
+      const alternatePath = path.get('alternate');
+      const alternate = alternatePath.value;
+      const test = alternate.test;
+
+      alternatePath.replace(
+        j.blockStatement([
+          j.expressionStatement(test),
+          j.ifStatement(test.left, alternate.consequent, alternate.alternate),
+        ]),
+      );
     });
 
   // Hoist assignments out of the first `if` and into the block body.
-  return sourceAST.find(j.IfStatement, {
-    test: {
-      type: j.AssignmentExpression.name,
-    },
-  }).forEach((path) => {
-    const assignment = path.get('test');
-    path.insertBefore(j.expressionStatement(assignment.node));
-    assignment.replace(assignment.node.left);
-  }).toSource(printOptions);
+  return sourceAST
+    .find(j.IfStatement, {
+      test: {
+        type: j.AssignmentExpression.name,
+      },
+    })
+    .forEach((path) => {
+      const assignment = path.get('test');
+      path.insertBefore(j.expressionStatement(assignment.node));
+      assignment.replace(assignment.node.left);
+    })
+    .toSource(printOptions);
 }
